e8fa0bde60f36f9074e82c4816f71385b1b5693a
ryicoh <ryicoh@gmail.com>
2025-06-30 22:53:39 +0900

    modified

--- flake.nix
+++ flake.nix
@@ -3,20 +3,226 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
-      name = "default";
-      paths = with nixpkgs.legacyPackages.aarch64-darwin; [
-        nodejs_22
-
-        postgresql_16_jit
-        mysql80
-
-        google-cloud-sdk
-        google-cloud-sql-proxy
-      ];
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nix-darwin,
+      home-manager,
+    }@inputs:
+    let
+      system = "aarch64-darwin";
+      user = "ryicoh";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+
+      formatter.${system} = nixpkgs.legacyPackages.${system}.nixfmt-rfc-style;
+
+      darwinConfigurations.default = nix-darwin.lib.darwinSystem {
+        system = system;
+        modules = [
+          {
+            nix = {
+              optimise.automatic = true;
+              settings = {
+                experimental-features = "nix-command flakes";
+                max-jobs = 8;
+              };
+            };
+
+            homebrew = {
+              enable = true;
+              onActivation = {
+                autoUpdate = true;
+                upgrade = true;
+                cleanup = "uninstall";
+              };
+              taps = [
+                "hashicorp/tap"
+                "homebrew/services"
+              ];
+              brews = [
+                "node@22"
+                "corepack"
+                "yarn"
+
+                "git-flow"
+
+                # "deno"
+                # "oven-sh/bun/bun"
+                "golang"
+                "rust"
+                "rustup"
+                # "dotnet"
+                # "coursier"
+                # "cmake"
+                "protobuf"
+                # "pkgconf"
+
+                # "planetscale/tap/pscale"
+                "hashicorp/tap/terraform"
+                "tflint"
+                "trivy"
+
+                "postgresql@16"
+                "mysql@8.0"
+
+                "cloud-sql-proxy"
+                "awscli"
+                "aws-cdk"
+
+                "ripgrep"
+                "fd"
+                "watch"
+                # "tldr"
+
+                # "poppler"
+
+                "kubernetes-cli"
+                "kubectx"
+                "stern"
+                "helm"
+                "kubeseal"
+                "gnupg"
+                "pinentry-mac"
+                "ykman"
+                "yubikey-personalization"
+
+                # "iperf"
+                # "iperf3"
+
+                # "ollama"
+                # "huggingface-cli"
+              ];
+              casks = [
+                "cloudflare-warp"
+                "gcloud-cli"
+                "keycastr"
+                # "libreoffice"
+                "cursor"
+                "datagrip"
+                "dbeaver-community"
+                "hot"
+                # "fanny"
+                # "istat-menus"
+                "aws-vpn-client"
+                "microsoft-edge"
+                "yubico-authenticator"
+                "yubico-yubikey-manager"
+                "eqmac"
+              ];
+            };
+
+            system = {
+              stateVersion = 5;
+              defaults = {
+                NSGlobalDomain = {
+                  "com.apple.trackpad.scaling" = 0.6875;
+                };
+                finder = {
+                  AppleShowAllFiles = true;
+                  AppleShowAllExtensions = true;
+                };
+                dock = {
+                  autohide = true;
+                  orientation = "right";
+                  tilesize = 25;
+                  magnification = true;
+                  largesize = 30;
+                  show-recents = true;
+                  persistent-apps = [
+                    "/Applications/Safari.app"
+                    "/Applications/Google Chrome.app"
+                    "/Applications/Cursor.app"
+                    "/Applications/DataGrip.app"
+                    "/System/Applications/Mail.app"
+                  ];
+                  persistent-others = [
+                    "/Users/${user}/Documents"
+                    "/Users/${user}/Downloads"
+                  ];
+                };
+                trackpad = {
+                  Clicking = true;
+                };
+                controlcenter = {
+                  BatteryShowPercentage = true;
+                  Bluetooth = true;
+                  Sound = true;
+                };
+                alf = {
+                  globalstate = 2;
+                };
+              };
+              keyboard = {
+                enableKeyMapping = true;
+                remapCapsLockToControl = true;
+              };
+            };
+
+            power = {
+              sleep = {
+                display = 15;
+              };
+            };
+
+            security = {
+              pam = {
+                services.sudo_local.touchIdAuth = true;
+              };
+            };
+          }
+        ];
+      };
+
+      homeConfigurations = {
+        default = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            {
+              home.stateVersion = "25.05";
+              home.username = user;
+              home.homeDirectory = "/Users/${user}";
+              home.file."/Users/${user}/.zprofile" = {
+                source = ./zprofile;
+                force = true;
+              };
+              home.file."/Users/${user}/.zshrc" = {
+                source = ./zshrc;
+                force = true;
+              };
+              home.file."/Users/${user}/.gnupg/gpg-agent.conf" = {
+                source = ./gpg-agent.conf;
+                force = true;
+              };
+              home.file."/Users/${user}/.config/git/config" = {
+                source = ./gitconfig;
+                force = true;
+              };
+              home.file."/Users/${user}/.config/git/ignore" = {
+                source = ./gitignore;
+                force = true;
+              };
+              home.file."/Users/${user}/Library/Application\ Support/Cursor/User/settings.json" = {
+                source = ./cursor_settings.json;
+                force = true;
+              };
+            }
+          ];
+        };
+      };
+    };
 }
